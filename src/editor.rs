e852875c4f2e20594c5c4655ea2cf23ef638cfcc
Eric <ericwu052@gmail.com>
2023-03-27 15:43:31 +0700

    add refresh screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -12,16 +12,27 @@ impl Editor {
 	let _stdout = stdout().into_raw_mode().unwrap();
 
 	loop {
-	    if let Err(error) = self.process_keypress() {
+	    if let Err(error) = self.refresh_screen() {
 		die(error);
 	    }
 	    if self.should_quit {
 		break;
 	    }
+	    if let Err(error) = self.process_keypress() {
+		die(error);
+	    }
 	}
     }
     pub fn default() -> Self {
 	Self { should_quit: false }
+    }
+    
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+	print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+	if self.should_quit {
+	    println!("Goodbye.\r");
+	}
+	io::stdout().flush()
     }
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
 	let pressed_key = read_key()?;
@@ -43,5 +54,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 }
 
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
